55a2097f191f5a0dc1f974d10e6695d7e4c0cf94
Toey <teayvvv5@gmail.com>
2016-01-10 14:06:22 +0700

    Show View Finish

--- app/src/main/java/kmutnb/khemmalakul/teay/mytraffic/DeatailActivity.java
+++ app/src/main/java/kmutnb/khemmalakul/teay/mytraffic/DeatailActivity.java
@@ -2,12 +2,49 @@ package kmutnb.khemmalakul.teay.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DeatailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deatail);
+
+        bindWidget();
+
+        //Shoe View
+        showView();
     } //main method
+
+    private void showView() {
+
+        //Show Title
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        //Show Image
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        trafficImageView.setImageResource(intIcon);
+
+        //Show Detail
+        int intIndex = getIntent().getIntExtra("Index", 0);
+        String[] detailString = getResources().getStringArray(R.array.detail_long);
+        detailTextView.setText(detailString[intIndex]);
+
+
+
+    }//showView
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.txtShowTitle);
+        detailTextView = (TextView) findViewById(R.id.txtShowDetail);
+        trafficImageView = (ImageView) findViewById(R.id.imvShowImage);
+    }//bindWidget
 }
