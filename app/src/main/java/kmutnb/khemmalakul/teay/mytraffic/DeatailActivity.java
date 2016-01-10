a6828fe36efa8b83d32f0c635da035972f21bd9d
Toey <teayvvv5@gmail.com>
2016-01-10 11:55:05 +0700

    Design UI Detail Finish

---
+++ app/src/main/java/kmutnb/khemmalakul/teay/mytraffic/DeatailActivity.java
@@ -0,0 +1,13 @@
+package kmutnb.khemmalakul.teay.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeatailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deatail);
+    }
+}
